839285b1616aaad15c1b15bb0dfd3df92898768b
Ashish Goyal <ashishgoyal247@gmail.com>
2018-09-03 23:54:55 +0530

    Book App using redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    {title: 'Goosebumps'},
+    {title: 'Javascript: The Good Parts'},
+    {title: 'Alchemist'},
+    {title: 'Harry Potter'},
+    {title: 'The Dark Tower'},
+  ];
+}
