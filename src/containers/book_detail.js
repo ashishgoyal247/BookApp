3a7e086f7c4fde34801d9758ba9d5a60da50a964
Ashish Goyal <ashishgoyal247@gmail.com>
2018-09-04 23:59:29 +0530

    Book detail view

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -3,8 +3,15 @@ import { connect } from 'react-redux';
 
 class BookDetail extends Component {
   render() {
+    if(!this.props.book) {
+      return <div>Start a book to get selected.</div>;
+    }
     return (
-      <div>Book Detail!</div>
+      <div>
+        <h3>Book Details of: </h3>
+        <div>Title: {this.props.book.title}</div>
+        <div>Pages: {this.props.book.pages}</div>
+      </div>
     );
   }
 }
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
   return [
-    {title: 'Goosebumps'},
-    {title: 'Javascript: The Good Parts'},
-    {title: 'Alchemist'},
-    {title: 'Harry Potter'},
-    {title: 'The Dark Tower'},
+    {title: 'Goosebumps', pages: 102},
+    {title: 'Javascript: The Good Parts', pages: 93},
+    {title: 'Alchemist', pages: 10},
+    {title: 'Harry Potter', pages: 39},
+    {title: 'The Dark Tower', pages: 33},
   ];
 }
